a7d8641f59821920402f84346577e29ef7e9dec6
talia <talias1235@gmail.com>
2021-12-08 09:36:14 +0200

    finish my_mat lib

--- my_mat.c
+++ my_mat.c
@@ -7,12 +7,50 @@
 #define n 10
 
 // insert values to a matrix from the user
-    void insertValues(int[n][n] mat){
+    void insertValues(int mat[n][n]){
+        int t;
+        int temp[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                scanf("%d", t);
+                mat[i][j] = t;
+                temp[i][j] = t;
 
+            }
+        }
+
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(i==j){
+                        mat[i][j]=0;
+                    }
+                    else if(temp[i][j] != 0 && temp[i][k] != 0 && temp[k][j] != 0){
+                        mat[i][j] = MIN(temp[i][j], temp[i][k] + temp[k][j]);
+                    }
+                    else if(temp[i][j] != 0 && (temp[i][k] == 0 || temp[k][j] == 0)){
+                        mat[i][j] = temp[i][j];
+                    }
+                    else if(temp[i][j] == 0 && temp[i][k] != 0 && temp[k][j] != 0){
+                        mat[i][j] = temp[i][k] + temp[k][j];
+                    }
+                    else{
+                        mat[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for (int j = 0; j < n; j++){
+                temp[i][j]=mat[i][j];
+            }
+        }
+        return 0;
     }
 
     // get two nodes to check if there is a path between them
-    int hasPath(int[n][n] mat, int i, int j){
+    int hasPath(int mat[n][n], int i, int j){
         if(mat[i][j] == 0){
             return 0;
         }
@@ -20,7 +58,7 @@
     }
 
     // get two nodes to return the shortest path between them
-    int shortestPath(int[n][n] mat, int i, int j){
+    int shortestPath(int mat[n][n], int i, int j){
         if(mat[i][j] == 0){
             return -1;
         }
