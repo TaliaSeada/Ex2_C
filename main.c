779fc2b8a6654f9bf477de3b90bd8b6cbe44f14f
talia <talias1235@gmail.com>
2021-12-08 09:48:54 +0200

    finish main

--- main.c
+++ main.c
@@ -5,11 +5,33 @@
 #define n 10
 
 int main(){
+    int mat[n][n];
+    char action = 0;
     
+    while(action != 'D'){
+        scanf("%c", action);
+        if(action == 'A'){
+            insertValues(mat);
+        }
+        
+        else if(action == 'B'){
+            int i, j;
+            scanf("%d%d", i,j);
+            int ans = hasPath(mat, &i, &j);
+            if(ans == 1){
+                printf("True\n");
+            }
+            else{
+                printf("False\n");
+            }
+        }
 
-
-
-
-
+        else if(action == 'C'){
+            int i, j;
+            scanf("%d%d", i,j);
+            int ans = shortestPath(mat, &i, &j);
+            printf("%d\n", ans);
+        }
+    }
     return 0;
 }
